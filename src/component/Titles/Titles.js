07294d5f3e5ba1868c86d1baa0add7793464fa5d
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 15:47:51 -0800

    Intial title page code

---
+++ src/component/Titles/Titles.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Titles.css';
+
+class Titles extends Component {
+    render() {
+        return(
+            <div>
+            <h1>Weather Finder</h1>
+            <p>Find out temperatures, conditions and more...</p>
+            </div>
+        );
+    }
+}
+
+export default Titles;
