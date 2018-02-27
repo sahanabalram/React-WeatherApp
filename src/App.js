2fcf7dcab3f1582acf4ea18bc925b8d2036e3c50
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 15:48:02 -0800

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Titles from './component/Titles';
+class App extends Component {
+  render() {
+    return(
+      <div>
+      <Titles/>
+      </div>
+    );
+  }
+}
+
+export default App;
