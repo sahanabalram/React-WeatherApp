d61ebdbab08976dd8406a948b7e23cd1f2225a5f
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 15:55:31 -0800

    Intial Weather component

---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Weather.css';
+
+class Weather extends Component {
+    render(){
+        return(
+            <div>Weather Component</div>
+        );
+    }
+}
+
+export default Weather;
