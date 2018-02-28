88c2c10cd9456f5419f2f6bff8dab0725994be54
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 16:53:01 -0800

    Display the details

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Weather.css';
 
 class Weather extends Component {
-    render(){
-        return(
-            <div>Weather Component</div>
+    render() {
+        return (
+            <div>
+                Location: {this.props.city}, {this.props.country}
+                Temperature: {this.props.temperature} 
+                Humidity:{this.props.humidity}
+                Conditions:{this.props.description}
+            </div>
         );
     }
 }
