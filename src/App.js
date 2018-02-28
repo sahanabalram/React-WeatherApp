e8b24aff6c5ed5220f2385233752e65bfdf19b5c
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 16:08:42 -0800

    Add the weather api key

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
+
+const apiKey = "adbf7a5641d4e0a71b928d138a4b6bed";
+
 class App extends Component {
+  getWeather =  async function() {
+    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=San Diego,unitedstates&appid=${apiKey}&unites=metric`);
+  }
   render() {
     return(
       <div>
