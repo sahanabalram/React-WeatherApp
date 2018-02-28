0d418fe50d9f0ffd1dcfc5d7f585f7462dd7d107
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 17:12:37 -0800

    Add check to see if the field city and country  are  empty or not

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
@@ -10,47 +10,59 @@ class App extends Component {
     super(props);
     // defining the initial state
     this.state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined
     }
   }
-  getWeather =  async (e) => {
+  getWeather = async(e) => {
     // signifies single page application
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    // variable that makes the call to the open weather map website for San Diego and United states
+    // variable that makes the call to the open weather map website for San Diego
+    // and United states
     const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}&unites=metric`);
     // convert the data that is received from the apicall to a json object
     const data = await apiCall.json();
-    console.log(data);
-    // update the value of temperature once the button(Get Weather) is clicked.
-    this.setState({
-      temperature:data.main.temp,
-      city:data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if (city === true && country === true) {
+      console.log(data);
+      // update the value of temperature once the button(Get Weather) is clicked.
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: ""
+      });
+    }
+
   }
   render() {
-    return(
+    return (
       <div>
-      <Titles/>
-      {/* A prop called getWeather is used to get all the information from getWeather function Access for getWeather function is now availabe on the Form component as well.*/}
-      <Form getWeather={this.getWeather}/>
-      <Weather 
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        error={this.state.error}/>
+        <Titles/> {/* A prop called getWeather is used to get all the information from getWeather function Access for getWeather function is now availabe on the Form component as well.*/}
+        <Form getWeather={this.getWeather}/>
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}/>
       </div>
     );
   }
