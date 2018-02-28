af20db0f886fa55d5f6c8fda286f0946c7f809c8
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 16:15:31 -0800

    Input a proper form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,11 @@ import './Form.css';
 class Form extends Component {
     render(){
         return(
-            <div>Form Component</div>
+            <form>
+                <input type="text" name="city" placeholder="City..."/>
+                <input type="text" name="country" placeholder="Country..."/>
+                <button>Get Weather</button>
+            </form>
         );
     }
 }
