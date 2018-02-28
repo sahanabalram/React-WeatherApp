db6d9e3cc9f5469153312a25beee9c4ba2e9ffc1
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 16:21:50 -0800

    Add onSubmit inbuilt method to the form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,7 +4,9 @@ import './Form.css';
 class Form extends Component {
     render(){
         return(
-            <form>
+            // When clicked on form button the inbuilt onSubmit method will fetch information from the weatherUrl which is avialable in the getWeather() function
+            
+            <form onSubmit={this.props.getWeather}>
                 <input type="text" name="city" placeholder="City..."/>
                 <input type="text" name="country" placeholder="Country..."/>
                 <button>Get Weather</button>
