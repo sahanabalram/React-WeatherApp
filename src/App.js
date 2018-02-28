653623387253b3e1cd2d31a681ba819857c63632
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 16:11:15 -0800

    Convert the data into json object

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ const apiKey = "adbf7a5641d4e0a71b928d138a4b6bed";
 
 class App extends Component {
   getWeather =  async function() {
+    // variable that makes the call to the open weather map website for San Diego and United states
     const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=San Diego,unitedstates&appid=${apiKey}&unites=metric`);
+    // convert the data that is received from the apicall to a json object
+    const data = await apiCall.json();
   }
   render() {
     return(
