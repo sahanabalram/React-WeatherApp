b4af957e9f4f57a9f840820497e1955b9397ba2a
Sahana Balram <sahana2008balram@gmail.com>
2018-02-27 17:05:11 -0800

    Display the paragraph information only after the get weather button is clicked in the Weather component

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -5,10 +5,14 @@ class Weather extends Component {
     render() {
         return (
             <div>
-                <p>Location: {this.props.city}, {this.props.country}</p>
-                <p>Temperature: {this.props.temperature} </p>
-                <p>Humidity:{this.props.humidity}</p>
-                <p>Conditions:{this.props.description}</p>
+            {/*if the city and country return true only then display the location paragraph once the the Get Weather button is clicked ( both the condition must be true)*/}
+                {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+                {/*if the temperature  return true only then display the temperature paragraph once the Get Weather button is clicked (the condition must be true)*/}
+                {this.props.temperature && <p>Temperature: {this.props.temperature} </p>}
+                 {/*if the humidity return true only then display the humidity once the Get Weather button is clicked (the condition must be true)*/}
+                {this.props.humidity && <p>Humidity:{this.props.humidity}</p>}
+                 {/*if the conditions return true only then display the conditions once the Get Weather button is clicked (the condition must be true)*/}
+                {this.props.description && <p>Conditions:{this.props.description}</p>}
             </div>
         );
     }
