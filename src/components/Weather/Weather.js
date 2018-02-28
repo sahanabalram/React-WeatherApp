a26f5796ff0d70dd2052df92538cc7e32aa857a5
Sahana Balram <sahana2008balram@gmail.com>
2018-02-28 15:11:40 -0800

    Add className to the span tag

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -7,32 +7,30 @@ class Weather extends Component {
             <div className="weather__info">
             {/*if the city and country return true only then display the location paragraph once the the Get Weather button is clicked ( both the condition must be true)*/}
                 {
-                    this.props.city && this.props.country && <p>Location: 
-                    <span>{this.props.city}, {this.props.country}</span>
+                    this.props.city && this.props.country && <p className="weather__key">Location: 
+                    <span className="weather__value">{this.props.city}, {this.props.country}</span>
                     </p>
                 }
                 
                 {
-                    this.props.temperature && <p>Temperature: 
-                   <span> {this.props.temperature} </span>
+                    this.props.temperature && <p className="weather__key">Temperature: 
+                   <span className="weather__value"> {this.props.temperature} </span>
                     </p>
                 }
              
                 {
-                    this.props.humidity && <p>Humidity:
-                    <span>{this.props.humidity}</span>
+                    this.props.humidity && <p className="weather__key">Humidity:
+                    <span className="weather__value">{this.props.humidity}</span>
                     </p>
                 }
                  
                 {
-                    this.props.description && <p>Conditions:
-                    <span>{this.props.description}</span>
+                    this.props.description && <p className="weather__key">Conditions:
+                    <span className="weather__value">{this.props.description}</span>
                     </p>
                 }
                 {
-                    this.props.error && <p>
-                    <span>{this.props.error}</span>
-                    </p> 
+                    this.props.error && <p>{this.props.error}</p> 
                 }
             </div>
         );
